80e5fd43e2f9d4e5d2a566ce98d37235886a7ebf
csak4718 <csak4718@yahoo.com.tw>
2014-05-18 23:47:01 +0800

    5/18: add comments

--- macho/requests_101.py
+++ macho/requests_101.py
@@ -3,11 +3,10 @@ import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 
 
-theurl='http://www.gap.com' 
-r = requests.get(theurl)
-#print r.text
+theurl='http://www.gap.com'
+r = requests.get(theurl)# Get a webpage, this creates a Response object called "r"
+# print r.text
 soup = BeautifulSoup(r.text)
 divs=soup.find_all('div')
 for div in divs:
 	print div
-
