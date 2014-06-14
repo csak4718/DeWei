fb60f4a5a49bf9475332c67d5daa27e5fc37bd64
csak4718 <csak4718@yahoo.com.tw>
2014-06-14 23:14:28 +0800

    6/14 Django tutorial part5 ends.

--- macho/myproject/polls/views.py
+++ macho/myproject/polls/views.py
@@ -1,11 +1,60 @@
-from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from polls.models import Poll, Choice
+from django.utils import timezone
 
 # Create your views here.
-from django.http import HttpResponse
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_poll_list'
 
-from polls.models import Poll
+	def get_queryset(self):
+		"""
+		Return the last five published polls (not including those set to be
+		published in the future).
+		"""
+		return Poll.objects.filter(
+			pub_date__lte=timezone.now()
+		).order_by('-pub_date')[:5]
 
+
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any polls that aren't published yet.
+        """
+        return Poll.objects.filter(pub_date__lte=timezone.now())
+
+
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
+
+def vote(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])# request.POST is a dictionary-like object that lets you access submitted data by key name. In this case, request.POST['choice'] returns the ID of the selected choice, as a string.
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the poll voting form.
+        return render(request, 'polls/detail.html', {
+            'poll': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+'''
 def index(request):
 	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5] # The negative sign in front of "-pub_date" indicates descending order. Ascending order is implied.
 	context = {'latest_poll_list': latest_poll_list}
@@ -20,7 +69,6 @@ def detail(request, poll_id):
 	return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
-
-def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/results.html', {'poll': poll})
+'''
